1037a81814065c3b61d6383b4f8547d5688e2938
Mairah <mairah.zulkepli@griffithuni.edu.au>
2019-09-19 02:50:14 +1000

    Added HexagonButton Class

---
+++ src/minesweeper/HexagonButton.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package minesweeper;
+import javax.swing.*;
+
+/**
+ *
+ * @author user
+ */
+public class HexagonButton extends JToggleButton {
+    private static final long serialVersionUID = 1L;
+    private static final int SIDES = 6;
+    private static final int SIDE_LENGTH = 50;
+    public static final int LENGTH = 95;
+    public static final int WIDTH = 105;
+    private int row = 0;
+    private int col = 0;
+}
