69d17c92aa3c2c35c59b988b16b6e9996017bf56
Mairah <mairah.zulkepli@griffithuni.edu.au>
2019-09-22 01:11:30 +1000

    Added HexLayout Class

---
+++ src/minesweeper/HexLayout.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package minesweeper;
+
+/**
+ *
+ * @author user
+ */
+public class HexLayout {
+    private final int levelData[][] = new int[5][5];
+    
+}
