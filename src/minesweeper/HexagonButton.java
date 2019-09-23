8ae4e45a5874a78ad77b37c4f5dbbdfafb6e2539
Mairah <mairah.zulkepli@griffithuni.edu.au>
2019-09-23 11:08:13 +1000

    Added drawHex function in HexagonButton Class

--- src/minesweeper/HexagonButton.java
+++ src/minesweeper/HexagonButton.java
@@ -12,13 +12,39 @@ import java.awt.*;
  * @author Mairah
  */
 public class HexagonButton extends JToggleButton {
+    public static boolean XYVertex=true;	//true: x,y are the co-ords of the first vertex.
     private static int BORDERS=50;	//default number of pixels for the border
+    private static int s=0;	// length of one side
+    private static int t=0;	// short side of 30o triangle outside of each hex
+    private static int r=0;	// radius of inscribed circle (centre to middle of each side). r= h/2
+    private static int h=0;	// height. Distance between centres of two adjacent hexes. Distance between two opposite sides in a hex.
     
     public static Polygon hex(int x0, int y0){
         int y = y0 + BORDERS;
         int x = x0 + BORDERS;
+        
+        if (s == 0  || h == 0) {
+            System.out.println("ERROR: size of hex has not been set");
+            return new Polygon();
+        }
+        
+        int[] cx,cy;
+        if (XYVertex) { 
+            cx = new int[] {x,x+s,x+s+t,x+s,x,x-t};  //this is for the top left vertex being at x,y. Which means that some of the hex is cutoff.
+        } else {
+            cx = new int[] {x+t,x+s+t,x+s+t+t,x+s+t,x+t,x};	//this is for the whole hexagon to be below and to the right of this point
+        }
+        cy = new int[] {y,y,y+r,y+r+r,y+r+r,y+r};
+        return new Polygon(cx,cy,6);
     }
     
+    public static void drawHex(int i, int j, Graphics2D g2){
+        int x = i*(s+t);
+        int y = j*h+(i%2) * h/2;
+        Polygon poly = hex(x,y);
+        g2.fillPolygon(poly);
+        g2.drawPolygon(poly);
+    }
     
 //    private static final long serialVersionUID = 1L;
 //    private static final int SIDES = 6;
