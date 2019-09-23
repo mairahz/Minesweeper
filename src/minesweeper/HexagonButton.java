fc98bc0a3e72796e0d8739ad2daa485b23e545a6
Mairah <mairah.zulkepli@griffithuni.edu.au>
2019-09-23 11:02:03 +1000

    Added hex function in HexagonButton Class

--- src/minesweeper/HexagonButton.java
+++ src/minesweeper/HexagonButton.java
@@ -12,40 +12,48 @@ import java.awt.*;
  * @author Mairah
  */
 public class HexagonButton extends JToggleButton {
-    private static final long serialVersionUID = 1L;
-    private static final int SIDES = 6;
-    private static final int SIDE_LENGTH = 50;
-    public static final int LENGTH = 95;
-    public static final int WIDTH = 105;
-    private int row = 0;
-    private int col = 0;
+    private static int BORDERS=50;	//default number of pixels for the border
     
-    public HexagonButton(int row, int col){
-        setContentAreaFilled(false);
-        setFocusPainted(true);
-        setBorderPainted(false);
-        setPreferredSize(new Dimension(WIDTH, LENGTH));
-        this.row = row;
-        this.col = col;
+    public static Polygon hex(int x0, int y0){
+        int y = y0 + BORDERS;
+        int x = x0 + BORDERS;
     }
     
-    @Override
-    public void paintComponent(Graphics g){
-        super.paintComponent(g);
-        Polygon hex = new Polygon();
-        for (int i = 0; i < SIDES; i++){
-            hex.addPoint((int) (50 + SIDE_LENGTH*Math.cos(i * 2 * Math.PI/SIDES)),
-                    (int) (50 + SIDE_LENGTH * Math.sin(i*2*Math.PI/SIDES)));
-        }
-        g.drawPolygon(hex);
-    }
     
-    public int getRow(){
-        return row;
-        
-    }
-    
-    public int getCol(){
-        return col;
-    }
+//    private static final long serialVersionUID = 1L;
+//    private static final int SIDES = 6;
+//    private static final int SIDE_LENGTH = 50;
+//    public static final int LENGTH = 95;
+//    public static final int WIDTH = 105;
+//    private int row = 0;
+//    private int col = 0;
+//    
+//    public HexagonButton(int row, int col){
+//        setContentAreaFilled(false);
+//        setFocusPainted(true);
+//        setBorderPainted(false);
+//        setPreferredSize(new Dimension(WIDTH, LENGTH));
+//        this.row = row;
+//        this.col = col;
+//    }
+//    
+//    @Override
+//    public void paintComponent(Graphics g){
+//        super.paintComponent(g);
+//        Polygon hex = new Polygon();
+//        for (int i = 0; i < SIDES; i++){
+//            hex.addPoint((int) (50 + SIDE_LENGTH*Math.cos(i * 2 * Math.PI/SIDES)),
+//                    (int) (50 + SIDE_LENGTH * Math.sin(i*2*Math.PI/SIDES)));
+//        }
+//        g.drawPolygon(hex);
+//    }
+//    
+//    public int getRow(){
+//        return row;
+//        
+//    }
+//    
+//    public int getCol(){
+//        return col;
+//    }
 }
